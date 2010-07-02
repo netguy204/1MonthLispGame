c8303760e82718defbc8215a496eb1181dd67ea9
Brian Taylor <el.wubo@gmail.com>
2010-07-02 16:44:24 -0400

    two tanks

--- src/MonthGame/core.clj
+++ src/MonthGame/core.clj
@@ -20,7 +20,20 @@
 (def animation-sleep-ms 50)
 
 ; oto = offset to origin center of mass - top left corner of sprite
-(defstruct tank-struct :angle :pos :oto :sprites :state)
+(defstruct tank-struct
+  :angle :pos :oto :sprites
+  :rotate-rate :move-rate
+  :move-energy :fire-energy :state)
+
+(defstruct world-struct
+  :tanks :current-tank)
+
+(defn get-current-tank [world]
+  (nth (:tanks world) (:current-tank world)))
+
+(defmacro with-each-tank [world var & forms]
+  `(dorun (for [~var (:tanks ~world)]
+	   ~@forms)))
 
 (defn num-frames [tank]
   (count (:sprites tank)))
@@ -28,14 +41,20 @@
 (defn frame-angle-tolerance [tank]
   (rad-per-frame (num-frames tank)))
 
-(defn make-tank [stream tgt-sz doto]
-  (let [frames (load-sprites stream tgt-sz)
-	width (.getWidth (first frames))
+(defn make-tank [frames doto angle pos]
+  (let [width (.getWidth (first frames))
 	height (.getHeight (first frames))
 	midsprite (list (/ width 2) (/ height 2))
-	oto (vadd midsprite doto)]
+	oto (vadd midsprite doto)
+	rotate-rate 0.01
+	move-rate 0.7
+	move-energy 100
+	fire-energy 200]
 
-    (struct tank-struct 0 midsprite oto frames :idle)))
+    (struct tank-struct 
+	    angle pos oto frames
+	    rotate-rate move-rate
+	    move-energy fire-energy :idle)))
 
 (defn draw-tank [g tank]
   (let [frame (angle-to-frame (:angle tank) (num-frames tank))
@@ -46,6 +65,10 @@
       (.setColor (. Color black))
       (draw-leader (:pos tank) 50 (:angle tank))
       (draw-circle (:pos tank) 50 30)
+      (.setColor (. Color red))
+      (draw-circle (:pos tank) (:fire-energy tank) 30)
+      (.setColor (. Color blue))
+      (draw-circle (:pos tank) (:move-energy tank) 30)
       (draw-img (nth (:sprites tank) frame) tgt))))
 
 (defstruct mouse-struct :pos :button1down :button2down :lastevent)
@@ -59,7 +82,7 @@
 	     (< new-angle 0) (+ new-angle twopi)
 	     true new-angle)))
 	   
-(defn my-draw [g this tank]
+(defn my-draw [g this world]
   (let [width (.getWidth this)
 	height (.getHeight this)
 	img (new BufferedImage width height
@@ -67,15 +90,23 @@
 	bg (.getGraphics img)
 	o (list 64 64)]
     (dosync
+     ;; clear backgroud
      (doto bg
        (.setColor (. Color green))
        (.fillRect 0 0 width height)
-       (draw-tank @tank))
-     (if (:pos @mouse)
-       (draw-circle bg (:pos @mouse) 30 30)))
+       (.setColor (. Color black)))
 
-    (.drawImage g img 0 0 nil)
-    (.dispose bg)))
+     ;; draw pointer
+     (if (:pos @mouse)
+       (draw-circle bg (:pos @mouse) 30 30))
+
+     ;; draw tanks
+     (with-each-tank @world tank
+       (draw-tank bg @tank)))
+
+    (.dispose bg)
+    (.drawImage g img 0 0 nil)))
+
 
 (defn update-click [mouse ev down]
   (let [button (.getButton ev)
@@ -116,16 +147,32 @@
      (> delta 0) (if (< delta pi) 1 -1)
      (< delta 0) (if (< delta (neg pi)) 1 -1)
      true 0)))
-     
+
+(defn subtract-energy [tank energy-type factor]
+  (assoc tank energy-type (- (energy-type tank) (* factor (:move-rate tank)))))
+
+(defn subtract-move-energy [tank factor]
+  (subtract-energy tank :move-energy factor))
+
+(defn subtract-fire-energy [tank factor]
+  (subtract-energy tank :fire-energy factor))
+
 (defn move-towards-cursor [tank mouse]
   (let [to-mouse (vsub (:pos mouse) (:pos tank))
 	tank-dir (unitdir (discretize-angle (:angle tank) (num-frames tank)))
-	dtheta (vang to-mouse tank-dir)]
+	dtheta (vang to-mouse tank-dir)
+	rotate-dir (dir-to-rotate tank-dir to-mouse)]
     (if (> (vmag to-mouse) 50)
       (if (within dtheta (frame-angle-tolerance tank))
-	(assoc tank :pos (vadd (:pos tank) (vmul tank-dir 0.7)))
-	(assoc tank :angle (add-on-circle (:angle tank)
-					  (* 0.01 (dir-to-rotate tank-dir to-mouse)))))
+	(-> tank
+	    (assoc :pos (vadd (:pos tank) (vmul tank-dir (:move-rate tank))))
+	    (subtract-move-energy 1)
+	    (subtract-fire-energy 2))
+	(-> tank
+	    (assoc :angle
+	      (add-on-circle (:angle tank)
+			     (* (:rotate-rate tank) rotate-dir)))
+	    (subtract-move-energy 0.1)))
       tank)))
 	
 (defn animate-tank [tank mouse]
@@ -133,22 +180,31 @@
     (move-towards-cursor tank mouse)
     tank))
   
-(defn animation [x panel tank]
+(defn animation [x panel world]
   (dosync
-   (alter tank animate-tank @mouse))
+   (alter (get-current-tank @world) animate-tank @mouse))
   (.repaint panel)
   (Thread/sleep animation-sleep-ms)
-  (send-off *agent* #'animation panel tank))
+  (send-off *agent* #'animation panel world))
+
+(defn get-resource [file]
+  (let [loader (clojure.lang.RT/baseLoader)]
+    (.getResourceAsStream loader file)))
+
+(def my-world (ref (struct world-struct)))
 
 (defn -main [& args]
-
-  (let [loader (clojure.lang.RT/baseLoader)
-	tank-img (.getResourceAsStream loader "MonthGame/tanksprite.png")
-	my-tank (ref (make-tank tank-img 128 '(0 15)))
-	
+  (let [img-stream (get-resource "MonthGame/tanksprite.png")
+	frames (load-sprites img-stream 128)
+	tank1 (ref (make-tank frames '(0 15) (/ Math/PI 4) '(64 64)))
+	tank2 (ref (make-tank frames '(0 15) (/ (* 2 Math/PI) 3) '(300 300)))
 	panel (proxy [JPanel] []
-		(paint [g] (my-draw g this my-tank)))
+		(paint [g] (my-draw g this my-world)))
 	main-window (JFrame. "Month Game")]
+
+    (dosync
+     (alter my-world assoc :tanks (list tank1 tank2) :current-tank 0))
+
     (doto panel
       (.addMouseListener mouse-adapter)
       (.addMouseMotionListener mouse-adapter)
@@ -157,7 +213,7 @@
     (doto main-window
       (.setSize 400 400)
       (.add panel)
-      (.setDefaultCloseOperation (. JFrame EXIT_ON_CLOSE))
+      ;(.setDefaultCloseOperation (. JFrame EXIT_ON_CLOSE))
       (.setVisible true))
 
-    (send-off animator animation panel my-tank)))
+    (send-off animator animation panel my-world)))
