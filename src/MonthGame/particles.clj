cdc161fa47b4e91fde35cc97284e5ed4d6e7d034
Brian Taylor <el.wubo@gmail.com>
2010-07-11 01:14:08 -0400

    much cooler looking smoke

--- src/MonthGame/particles.clj
+++ src/MonthGame/particles.clj
@@ -3,26 +3,46 @@
 	MonthGame.vector
 	MonthGame.util
 	MonthGame.draw
-	MonthGame.scalar-math))
+	MonthGame.scalar-math
+	MonthGame.sprite))
 
-(import '(java.awt Color Graphics Dimension))
+(import '(java.awt Color Graphics Dimension
+		   AlphaComposite))
 
 (def *drag-factor* 0.9)
 
+(def *max-age* 2)
+
 (defn simple-drag [p world dt-secs]
-  (if (> (:age p) 2) nil
+  (if (> (:age p) *max-age*) nil
       (let [old-pos (position p)
 	    old-vel (:vel p)
 	    pos (vadd old-pos (vmul old-vel dt-secs))
 	    vel (vsub old-vel (vmul old-vel (* *drag-factor* dt-secs)))]
 	(assoc p :pos pos :vel vel))))
 
+(def *smoke-particles*
+     (map #(scale-img (load-img (get-resource "MonthGame/smoke.png")) % %)
+	  (range 32 128)))
+
+(defmacro with-new-graphics [graphics & forms]
+  `(let [~graphics (.create ~graphics)
+	 result# (do ~@forms)]
+     (.dispose ~graphics)
+     result#))
+
 (defn simple-draw [p g]
-  (let [pt1 (vint (position p))
-	pt2 (vadd pt1 [3 3])]
-    (doto g
-      (.setColor (. Color gray))
-      (fill-rect pt1 pt2))))
+  (let [age-factor (max 0.0 (min 1.0 (/ (:age p) *max-age*)))
+	alpha (- 1 age-factor)
+	num-frames (count *smoke-particles*)
+	frameno (int (min (dec num-frames) (max 0 (* num-frames age-factor))))
+	frame (nth *smoke-particles* frameno)
+	pos (vint (vsub (position p) (middle-img frame)))
+	comp (AlphaComposite/getInstance AlphaComposite/SRC_OVER alpha)]
+    (with-new-graphics g
+      (doto g
+	(.setComposite comp)
+	(draw-img frame pos)))))
 
 (defrecord Particle
   [pos vel update-fn draw-fn]
@@ -47,13 +67,15 @@
 
 (derive Particle :MonthGame.entity/non-interacting-npe)
 
-(def *perp-vel-scale* 0.3)
+(def *perp-vel-scale* 25.0)
+(def *forward-vel* 5.0)
+(def *offset-backward* 40)
 
 (defn emit-basic-particle [pos vel]
   "emit a particle in vel direction with a random perpendicular velocity"
-  (let [vel (vmul vel 0.5)
-	vp (vmul (vperp vel)
-		 (* (vmag vel)
-		    (rand-around-zero *perp-vel-scale*)))
-	vel (vadd vel vp)]
+  (let [dir (unit-vector vel)
+	vp (vmul (vperp vel) (rand-around-zero *perp-vel-scale*))
+	vel (vadd (vmul dir *forward-vel*) vp)
+	pos (vsub pos (vmul dir *offset-backward*))]
+
     (Particle. pos vel simple-drag simple-draw)))
